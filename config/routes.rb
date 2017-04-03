007a4d4b8603ceba041f108911ee80859edd2461
Michael Fashola <michaelfashola@gmail.com>
2017-04-03 11:25:37 -0400

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
-    resources :doctors, constraints: { format: 'json' } do
+    resources :doctors, only: [:index, :create, :update, :destroy, :show] do
       resources :reviews, only: [:index, :create, :update, :destroy, :show]
     end
   end
